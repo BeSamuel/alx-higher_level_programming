eba094a64feb1ebb6fe3a171b14cd339beb4f642
BeSamuel <samuelasefa27@gmail.com>
2023-03-06 10:33:47 -0800

    6-concat.py

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-str1 = "Holberton"
-str2 = "School"
-str1 += " " + str2
-print("Welcome to {}!".format(str1))
+word = "Holberton"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
